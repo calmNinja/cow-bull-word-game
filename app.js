35aa59c0c9ac56d6a6b93384299c8551e6e69f14
calmNinja <127276086+calmNinja@users.noreply.github.com>
2023-09-02 19:43:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -78,9 +78,7 @@ app.get("/check-dictionary", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.listen(PORT, () =>
-//   console.log(`Server running on http://localhost:${PORT}`)
-// );
-app.listen(PORT, '0.0.0.0', () =>
-  console.log(`Server running on http://0.0.0.0:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
 );
+
